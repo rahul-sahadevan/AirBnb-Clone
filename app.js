93b4ec14931259acc379b172b4f4fedc5b084580
rahul-sahadevan <rahulsahadevan000@gmail.com>
2023-10-05 21:36:54 +0530

    notfinshedAirbnb

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ document.addEventListener('DOMContentLoaded', function() {
                     searchVal:searchInput.value
                 }
                 localStorage.setItem("obj",JSON.stringify(obj));
-                window.location.href = "/listing.html"
+                window.location.href = "listing.html"
             } 
             catch (error) {
                 console.error(error);
